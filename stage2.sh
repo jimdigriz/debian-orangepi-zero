3b118706ec2171f9697780fede9413e4fb8bf8ce
Alexander Clouter <alex@digriz.org.uk>
2019-04-18 23:42:09 +0100

    more networking (and permit root on ttyGS0 console)

--- stage2.sh
+++ stage2.sh
@@ -32,6 +32,7 @@ find /var/lib/apt/lists -type f -delete
 passwd -d root
 
 echo g_serial >> /etc/modules
+printf "# USB Serial Gadget\nttyGS0\n" >> /etc/securetty
 systemctl enable serial-getty@ttyGS0.service
 
 cat <<'EOF' > /etc/fstab
@@ -43,6 +44,26 @@ cat <<'EOF' > /etc/fstab
 #
 # <file system>	<mount point>	<type>	<options>		<dump>	<pass>
 LABEL=root	/		auto	errors=remount-ro	0	1
+EOF
+
+cat <<'EOF' > /etc/network/interfaces
+# This file describes the network interfaces available on your system
+# and how to activate them. For more information, see interfaces(5).
+
+source /etc/network/interfaces.d/*
+
+# The loopback network interface
+auto lo
+iface lo inet loopback
+
+iface default inet dhcp
+EOF
+
+mkdir /etc/network/interfaces.d
+
+cat <<'EOF' > /etc/network/interfaces.d/eth0
+allow-hotplug eth0
+iface eth0 inet dhcp
 EOF
 
 exit 0
