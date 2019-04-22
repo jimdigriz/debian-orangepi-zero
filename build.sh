6eaf42eb247835bae16b5b36dd01e0a6adccbee2
Alexander Clouter <alex@digriz.org.uk>
2019-04-22 14:20:29 +0100

    hush

--- build.sh
+++ build.sh
@@ -18,6 +18,6 @@ docker build -t opi0-stage3 -f Dockerfile.stage3 .
 ID=$(docker create --cap-add SYS_ADMIN --privileged -v /dev/:/dev opi0-stage3)
 docker start -a $ID
 docker cp $ID:debian-orange-pi-zero.img .
-docker rm $ID
+docker rm $ID >/dev/null
 
 exit 0
