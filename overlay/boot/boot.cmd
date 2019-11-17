c76c33bfd5fc51115a29c3b162619b6e61ffcd47
Alexander Clouter <alex@digriz.org.uk>
2019-11-17 10:34:25 +0000

    yes to fsck

--- overlay/boot/boot.cmd
+++ overlay/boot/boot.cmd
@@ -1,5 +1,5 @@
 ext4load mmc 0 ${fdt_addr_r} /boot/${fdtfile}
 ext4load mmc 0 ${kernel_addr_r} vmlinuz
 ext4load mmc 0 ${ramdisk_addr_r} initrd.img
-setenv bootargs console=ttyS0,115200 console=ttyGS0,115200 earlyprintk initrd=${ramdisk_addr_r},32M root=/dev/mmcblk0p1 panic=10
+setenv bootargs console=ttyS0,115200 console=ttyGS0,115200 earlyprintk initrd=${ramdisk_addr_r},32M root=/dev/mmcblk0p1 fsck.repair=yes panic=10
 bootz ${kernel_addr_r} - ${fdt_addr_r}
